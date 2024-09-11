f5ea78b636f5d43452d3f3b5a7286f4cb48dd978
turbocep <162988238+turbocep@users.noreply.github.com>
2024-09-11 14:00:54 +0200

    write legislator letters to folder.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,31 +1,25 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
+require 'erb'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
 def legislators_by_zipcode(zipcode)
-  #Creates new service instance:
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
-  #Fetches API key into service:
   civic_info.key = File.read('key.txt')
 
   begin
-    legislators = civic_info.representative_info_by_address(
+    civic_info.representative_info_by_address(
       address: zipcode,
       levels: 'country',
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-    legislators = legislators.officials
-    legislator_names = legislators.map(&:name)
-    legislator_names.join(", ")
+    ).officials
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 end
-
-  
 
 puts 'EventManager initialized.'
 
@@ -35,14 +29,32 @@ contents = CSV.open(
   header_converters: :symbol
 )
 
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
 
   zipcode = clean_zipcode(row[:zipcode])
 
   legislators = legislators_by_zipcode(zipcode)
 
-  puts "#{name} #{zipcode} #{legislators}"
+  form_letter = erb_template.result(binding)
+  puts form_letter
+
+  Dir.mkdir('output') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filename, 'w') do |file|
+    file.puts form_letter
+  end
 end
+
+
+
+
+
 
 
