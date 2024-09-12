9685aa45eff4affc40a534acd56dd591bbb92d82
turbocep <162988238+turbocep@users.noreply.github.com>
2024-09-12 13:35:48 +0200

    create clean phone number method.

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,26 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+#Need to first count digits of phone number in the way it's formatted. Convert to number. 
+
+def format_home_phone(home_phone)
+  home_phone.split("").reject do |num|
+    ['-', '(', ')', ' ', '+', '.'].include?(num)
+  end.join('')
+end
+
+def clean_home_phone(home_phone)
+  cleaned_phone = format_home_phone(home_phone)
+  phone_length = cleaned_phone.length
+  if phone_length == 10
+    return cleaned_phone.to_i
+  elsif phone_length == 11 && home_phone[0] == '1'
+    return cleaned_phone[1..11].to_i
+  else
+    return 0
+  end
+end
+
 def legislators_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = File.read('key.txt')
@@ -21,28 +41,7 @@ def legislators_by_zipcode(zipcode)
   end
 end
 
-puts 'EventManager initialized.'
-
-contents = CSV.open(
-  'event_attendees.csv',
-  headers: true,
-  header_converters: :symbol
-)
-
-template_letter = File.read('form_letter.erb')
-erb_template = ERB.new template_letter
-
-contents.each do |row|
-  id = row[0]
-  name = row[:first_name]
-
-  zipcode = clean_zipcode(row[:zipcode])
-
-  legislators = legislators_by_zipcode(zipcode)
-
-  form_letter = erb_template.result(binding)
-  puts form_letter
-
+def save_thank_you_letter(id, form_letter)
   Dir.mkdir('output') unless Dir.exist?('output')
 
   filename = "output/thanks_#{id}.html"
@@ -50,6 +49,31 @@ contents.each do |row|
   File.open(filename, 'w') do |file|
     file.puts form_letter
   end
+end
+
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+p contents.headers
+
+template_letter = File.read('form_letter.erb')
+erb_template = ERB.new template_letter
+
+contents.each do |row|
+  id = row[0]
+  name = row[:first_name]
+  phone = clean_home_phone(row[:homephone])
+  p phone
+  zipcode = clean_zipcode(row[:zipcode])
+  legislators = legislators_by_zipcode(zipcode)
+
+  form_letter = erb_template.result(binding)
+
+  save_thank_you_letter(id, form_letter)
 end
 
 
--- test.rb
+++ test.rb
@@ -1,11 +1,21 @@
-require 'erb'
+def format_home_phone(home_phone)
+  home_phone.to_s.split("").reject do |num|
+    ['-', '(', ')', ' ', '+', '.'].include?(num)
+  end.join('')
+end
 
+def clean_home_phone(home_phone)
+  cleaned_phone = format_home_phone(home_phone)
+  phone_length = cleaned_phone.length
+  if phone_length == 10
+    return home_phone
+  elsif phone_length == 11 && home_phone.first == '1'
+    return home_phone[1..11]
+  else
+    return ''
+  end
+end
 
-meaning_of_life = 42
+home_phone = :6154385000
 
-question = "The Answer to the Ultimate Question of Life, the Universe, and Everything is <%= meaning_of_life %>"
-
-template = ERB.new question
-
-results = template.result(binding)
-puts results
+p clean_home_phone(home_phone)
